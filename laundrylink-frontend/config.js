d6ce4c3139add6b2888ce5ca4fa121fa6b7b6fbe
itskairosbtw <itskairosbtw@gmail.com>
2026-06-21 22:38:18 +0800

    night

--- laundrylink-frontend/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// Edit this file for each shop deployment
-const CONFIG = {
-  PI_BASE_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:') 
-    ? 'http://127.0.0.1:5000/api' 
-    : window.location.origin + '/api',
-  LOCATION_ID: "local",
-  SHOP_NAME: "LaundryLink",
-};
